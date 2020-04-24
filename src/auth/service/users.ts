3120f015a8e55a4c9531d105b4c8a65c4bf4f088
hore1014 <hore1014@users.noreply.github.com>
2020-04-24 18:28:11 +0200

    users and roles for authentication

--- src/auth/service/users.ts
+++ src/auth/service/users.ts
@@ -1,20 +1,3 @@
-/*
- * Copyright (C) 2020 - present Juergen Zimmermann, Hochschule Karlsruhe
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 import type { User } from './user.service';
 
 export const users: Array<User> = [
@@ -23,46 +6,46 @@ export const users: Array<User> = [
         username: 'admin',
         password:
             '$2b$12$jC0vLwqFtvikW9hrKL4RZ.ZMeaW5r/jYrNkm.ub23bMWL/mE2O20O',
-        email: 'admin@acme.com',
-        roles: ['admin', 'mitarbeiter', 'abteilungsleiter', 'kunde'],
+        email: 'admin@auto.com',
+        roles: ['admin', 'mitarbeiter', 'kunde'],
     },
     {
         _id: '20000000-0000-0000-0000-000000000002',
-        username: 'adriana.alpha',
+        username: 'brenna.olson',
         password:
             '$2b$12$jC0vLwqFtvikW9hrKL4RZ.ZMeaW5r/jYrNkm.ub23bMWL/mE2O20O',
-        email: 'adriana.alpha@acme.com',
+        email: 'brenna.olson@auto.com',
         roles: ['admin', 'mitarbeiter', 'kunde'],
     },
     {
         _id: '20000000-0000-0000-0000-000000000003',
-        username: 'alfred.alpha',
+        username: 'debra.knott',
         password:
             '$2b$12$jC0vLwqFtvikW9hrKL4RZ.ZMeaW5r/jYrNkm.ub23bMWL/mE2O20O',
-        email: 'alfred.alpha@acme.com',
+        email: 'debra.knott@auto.com',
         roles: ['mitarbeiter', 'kunde'],
     },
     {
         _id: '20000000-0000-0000-0000-000000000004',
-        username: 'anton.alpha',
+        username: 'kevin.richard',
         password:
             '$2b$12$jC0vLwqFtvikW9hrKL4RZ.ZMeaW5r/jYrNkm.ub23bMWL/mE2O20O',
-        email: 'anton.alpha@acme.com',
+        email: 'kevin.richard@auto.com',
         roles: ['mitarbeiter', 'kunde'],
     },
     {
         _id: '20000000-0000-0000-0000-000000000005',
-        username: 'dirk.delta',
+        username: 'dominik.brandt',
         password:
             '$2b$12$jC0vLwqFtvikW9hrKL4RZ.ZMeaW5r/jYrNkm.ub23bMWL/mE2O20O',
-        email: 'dirk.delta@acme.com',
+        email: 'dominik.brandt@auto.com',
         roles: ['kunde'],
     },
     {
         _id: '20000000-0000-0000-0000-000000000006',
-        username: 'emil.epsilon',
+        username: 'richard.wyatt',
         password:
             '$2b$12$jC0vLwqFtvikW9hrKL4RZ.ZMeaW5r/jYrNkm.ub23bMWL/mE2O20O',
-        email: 'emil.epsilon@acme.com',
+        email: 'richard.wyatt@auto.com',
     },
 ];
